e3228a568c5f2f3707a53422c5d5f21b2976fdbe
Archana Byagari <byagariarchana440@gmail.com>
2025-08-04 11:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll('.image img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'block';
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
